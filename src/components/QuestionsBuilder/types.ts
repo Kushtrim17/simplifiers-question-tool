310bfbf1b0d2a318de84a8ec36595292f6c9978b
Kateryna Zviagina <kateryna.zviagina@fortnox.se>
2025-09-08 15:16:04 +0200

    SI-368 Add document reference "section" type option

--- src/components/QuestionsBuilder/types.ts
+++ src/components/QuestionsBuilder/types.ts
@@ -39,7 +39,7 @@ export type ValueReference = {
 
 export type DocumentReference = {
   tableId: string; // 'managementReport'
-  type: "row" | "column" | "";
+  type: "row" | "column" | "section" | "";
   id: string; // '3'
   triggerAnswer: "yes" | "no" | ""; // 'yes'
 };
